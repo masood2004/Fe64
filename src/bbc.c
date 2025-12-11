122e997f5313a94fc06f248d6ab0840db53c603a
Syed Masood Hussain <hmasood3288@gmail.com>
2025-12-11 18:11:59 +0500

    Print Ranks and Files

--- src/bbc.c
+++ src/bbc.c
@@ -1,7 +1,60 @@
+// ============================================ \\
+
+// system headers
 #include <stdio.h>
 
+// defining bitboard data type
+#define U64 unsigned long long 
+
+
+// ============================================ \\
+
+// set/get/pop bit macros
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+
+
+// print board
+
+void print_bitboard(U64 bitboard){
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // Print Ranks
+            if(!file){
+                printf("  %d ", (8 - rank));
+            }
+
+            int square = rank * 8 + file;
+            // print bit state (Either 1 or 0)
+            printf(" %d", get_bit(bitboard, square) ? 1 : 0);
+        }
+        // print new line every rank
+        printf("\n");
+        }
+    // Print Files
+    printf("\n     a b c d e f g h \n\n");
+}
+
+        
+        
+// ============================================ \\
+
+
+
+
+
+
+
+
+
+
+
+
 int main(){
-    printf("bbc\n");
-    getchar();
+    U64 bitbaord = 3ULL;
+    print_bitboard(bitbaord);
     return 0;
 }
