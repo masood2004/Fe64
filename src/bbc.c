ca1ff4ba01198b19d7c5caa4eaef26e9e7a5c9dc
Syed Masood Hussain <hmasood3288@gmail.com>
2025-12-12 01:38:34 +0500

    implement set and pop bit function

--- src/bbc.c
+++ src/bbc.c
@@ -6,16 +6,44 @@
 // defining bitboard data type
 #define U64 unsigned long long 
 
+// Board Coordinates
+enum{
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1,
+};
+
+/*
+"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+
+*/
 
 // ============================================ \\
 
 // set/get/pop bit macros
-#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+#define get_bit(bitboard, square) (bitboard & (1ULL << square)) // for feteching bit/piece
+#define set_bit(bitboard, square) (bitboard |= (1ULL << square)) // For adding bit/piece
+#define pop_bit(bitboard, square) (get_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0) // For removing bit/piece
 
 
 // print board
 
 void print_bitboard(U64 bitboard){
+
+    printf("\n");
+
     // loop over board ranks
     for (int rank = 0; rank < 8; rank++)
     {
@@ -36,6 +64,9 @@ void print_bitboard(U64 bitboard){
         }
     // Print Files
     printf("\n     a b c d e f g h \n\n");
+
+    // Print bitboard as unsigned decimal number
+    printf("      Bitboard: %llud\n\n", bitboard);
 }
 
         
@@ -45,16 +76,24 @@ void print_bitboard(U64 bitboard){
 
 
 
-
-
-
-
-
-
-
-
 int main(){
-    U64 bitbaord = 3ULL;
-    print_bitboard(bitbaord);
+
+    // Define Bitboard
+    U64 bitboard = 0ULL;
+
+    // Setting bits
+    set_bit(bitboard, e2);
+    set_bit(bitboard, e1);
+
+    // Printing BitBoard
+    print_bitboard(bitboard);
+    
+    // Setting Pop Bit
+    pop_bit(bitboard, e2);
+    
+    // Printing BitBoard
+    print_bitboard(bitboard);
+
+
     return 0;
 }
